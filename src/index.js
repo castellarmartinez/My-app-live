d39aba5f8aa8a0f5bcd9368447adc5bbfb34d3fc
David Castellar Martínez <castellardav@gmail.com>
2022-04-08 09:30:48 -0500

    Serves static file to fetch products

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 require('./redis/redis-server')
 require('./db/mongoose')
 const express = require('express')
+const path = require('path')
 const swaggerJsDoc = require('swagger-jsdoc')
 const swaggerUI = require('swagger-ui-express')
 const swaggerOptions = require('./utils/swaggerOptions')
@@ -19,6 +20,7 @@ const app = express()
 
 app.use(express.json())
 app.use(helmet({ contentSecurityPolicy: { directives: cspDefaults }}))
+app.use(express.static(path.join(__dirname, './public')));
 
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpecs))
 app.use('/users', require('./routes/users-route'))
